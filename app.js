4e382f877cbb815204a666178e5c3294e8eca707
sara rahmani <sararahmani96@gmail.com>
2023-01-04 14:50:46 -0800

    Add a get and post request

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ app.get("/", (req, res) => {
   console.log("GET /")
   res.send("<h1>Hello aws</h1>")
 })
+const pokemons = [
+  {
+    id: 1,
+    name: "Pikachu",
+    type: "electric ⚡️",
+    level: 99,
+    image: "/pikachu.webp"
+  }
+]
 
+app.get("/api/pokemons", (req, res) => {
+  console.log("GET /api/pokemons")
+  res.send({pokemons: pokemons})
+});
+
+app.post("/api/pokemons", (req, res) => {
+  const data = req.body
+  console.log("POST /api/pokemons", data)
+  data.id = pokemons.length+1
+  pokemons.push(data)
+  res.send(data)
+})
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`listening on port ${port}`))
